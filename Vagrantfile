4a89a95b4b10d84ba15d08626d293f2bd8181c3c
discoposse <eric@discoposse.com>
2016-01-03 16:58:16 -0500

    adding rvm stuff to build script

--- Vagrantfile
+++ Vagrantfile
@@ -9,6 +9,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     jekyllsandbox.vm.hostname = "jekyllsandbox"
     jekyllsandbox.vm.box = "trusty-server"
     jekyllsandbox.vm.box_url = "https://oss-binaries.phusionpassenger.com/vagrant/boxes/latest/ubuntu-14.04-amd64-vbox.box"
+    jekyllsandbox.vm.network :private_network, ip: "10.180.0.10", :netmask => "255.255.255.0"
     jekyllsandbox.vm.provision :shell, :path => "build.sh"
   end
 end
--- build.sh
+++ build.sh
@@ -8,14 +8,20 @@ export DEBIAN_FRONTEND=noninteractive
 echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
 
 sudo apt-get update && sudo apt-get upgrade -y 
-sudo apt-get install -y git git-review vim openssh-server && sudo reboot
+sudo apt-get install -y git git-review vim openssh-server 
 
 # Install RVM to get updated Ruby and use 2.0.0 for vagrant user
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+sudo apt-get install -y nodejs
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+sudo curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /home/vagrant/.rvm/scripts/rvm
 rvm get stable
-rvm install 2.0.0
+rvm install 2.1.1
+rvm use 2.1.1
 rvm rubygems latest
 gem install bundler
+
+
+
 
