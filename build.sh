e7467085ca7ea79e75c3c7905015af7d99c1cfba
discoposse <eric@discoposse.com>
2016-01-03 14:40:33 -0500

    adding rvm and ruby for Jekyll dependencies

--- build.sh
+++ build.sh
@@ -9,3 +9,13 @@ echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
 
 sudo apt-get update && sudo apt-get upgrade -y 
 sudo apt-get install -y git git-review vim openssh-server && sudo reboot
+
+# Install RVM to get updated Ruby and use 2.0.0 for vagrant user
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+rvm get stable
+rvm install 2.0.0
+rvm rubygems latest
+gem install bundler
+
